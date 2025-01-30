399083019611632b79fcd854cabb8a28ce19078d
Vihaan Philip <vihaanphilip@gmail.com>
2025-01-30 12:11:09 +0800

    first commit
    playing around with JS

---
+++ variables.js
@@ -0,0 +1,10 @@
+let message = 'Hello';
+message = 'World!';
+console.log(message); // World!
+
+const CHRISTMAS_2025 = '2025-12-25';
+
+CHRISTMAS_2025 = '2025-12-26'; // TypeError: Assignment to constant variable.
+
+console.log(christmas_2025); // 2025-12-25
+
